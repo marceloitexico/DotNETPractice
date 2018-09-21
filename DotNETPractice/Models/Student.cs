0eeb54f6b1bc5bf55de63507a93966faa62facb3
marceloitexico <33935600+marceloitexico@users.noreply.github.com>
2018-09-21 11:04:19 -0500

    ssadasd asd

--- DotNETPractice/Models/Student.cs
+++ DotNETPractice/Models/Student.cs
@@ -13,7 +13,6 @@ namespace EFApproaches.DAL.Entities
         {
             FirstMidName = firstMidName;
             LastName = lastName;
-            FullName = firstMidName + " " + lastName;
         }
         public int ID { get; set; }
         [Required]
@@ -23,7 +22,13 @@ namespace EFApproaches.DAL.Entities
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime EnrollmentDate { get; set; }
         public  string EmailAddress { get; set; }
-        public string FullName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstMidName + " " + LastName;
+            }
+        }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
 
         public virtual void GenerateEmailFromName(string domain)
